905bd50e5bcaa3659adc1b5816164d5e6679cabb
Selasi3 <selasiayittah3@gmail.com>
2023-08-12 07:11:03 +0000

    restructured the project

---
+++ src/components/StarsDisplay.js
@@ -0,0 +1,17 @@
+import React from "react";
+import utils from "../math-utils";
+
+
+const StarsDisplay = (props) => (
+
+    <>
+      {utils.range(1, props.count).map(starId =>
+      <div key={starId} className='star' />
+      )}
+    </>
+
+
+  )
+
+export default StarsDisplay;
+
